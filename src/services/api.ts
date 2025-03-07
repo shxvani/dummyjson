f1a6c9312367229962f785636bef3c4d93a0e5d1
Shivani S <SESA755125@se.com>
2025-03-07 15:35:08 +0530

    styles

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 // src/services/api.ts
 export async function fetchProducts(limit: number, skip: number) {
-  const response = await fetch(`https://dummyjson.com/products?limit=30&skip=${skip}`);
+  const response = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`);
   const data = await response.json();
   return data.products;
 }
@@ -13,7 +13,7 @@ export async function fetchProductById(id: number) {
 
 export async function searchProducts(query: string, limit: number, skip: number) {
   console.log('Search query:', query); // Log the query before making the fetch call
-  const response = await fetch(`https://dummyjson.com/products/search?q=${query}&limit=30&skip=${skip}`);
+  const response = await fetch(`https://dummyjson.com/products/search?q=${query}&limit=${limit}&skip=${skip}`);
   const data = await response.json();
   console.log('API response:', data);
   return data.products;
